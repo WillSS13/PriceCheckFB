02f9cb5a738747475625ffbf74967babcd8fd133
Will Squibb <wsquibb@andrew.cmu.edu>
2024-10-01 12:43:41 -0400

    Init

---
+++ PriceCheckFB/DataRepositories/LocationRepository.swift
@@ -0,0 +1,36 @@
+// Created by Prof. H in 2022
+// Part of the PriceCheckFB project
+// Using Swift 5.0
+// Qapla'
+
+
+import Combine
+// import Firebase modules here
+import Firebase
+import FirebaseFirestore
+
+class LocationRepository: ObservableObject {
+  // Set up properties here
+  let path: String = "location_scans"
+  let store = Firestore.firestore()
+  
+  @Published var locations: [Location] = []
+  
+  init() {
+    get()
+  }
+
+  func get() {
+    // Complete this function
+    store.collection(path)
+      .addSnapshotListener { querySnapshot, error in
+        if let error {
+          print("Error: \(error.localizedDescription)")
+          return
+        }
+        self.locations = querySnapshot?.documents.compactMap { document in
+          try? document.data(as: Location.self)
+        } ?? []
+      }
+  }
+}
